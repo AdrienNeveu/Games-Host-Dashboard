f67ee084b082cd93fe0205108cee728fa9ea22f2
Adrien Neveu <adrien.neveu@epita.fr>
2017-03-09 22:10:50 +0100

    Add VueJS + Webpack + Bootstrap template

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <pageHeader></pageHeader>
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'games-host-dashboard'
+    }
+  },
+  components: {
+    pageHeader: require('./components/BaseTemplate/Header')
+  }
+}
+</script>
